f20416a84940192c3f6cb9788ab8e3f101b9efc9
jeonggiju <halogiju123@gmail.com>
2025-12-30 20:12:21 +0900

    add: @Configuration annotation to ScheduleConfig
    - Enabled Spring configuration by adding the @Configuration annotation.

--- src/main/java/com/mopl/mopl_batch/batch/config/ScheduleConfig.java
+++ src/main/java/com/mopl/mopl_batch/batch/config/ScheduleConfig.java
@@ -1,7 +1,9 @@
 package com.mopl.mopl_batch.batch.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@Configuration
 @EnableScheduling
 public class ScheduleConfig {
 }
