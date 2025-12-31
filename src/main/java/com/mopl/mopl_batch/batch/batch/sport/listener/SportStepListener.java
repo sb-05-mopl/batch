ed83dedd03c7e6d569cda13151156195ea6a3b9b
jeonggiju <halogiju123@gmail.com>
2025-12-31 20:18:36 +0900

    refactor: remove unused Type dependency from SportStepListener
    - Cleaned up unused `Type` field and related log messages.
    - Improved readability and reduced unnecessary code in the listener.

--- src/main/java/com/mopl/mopl_batch/batch/batch/sport/listener/SportStepListener.java
+++ src/main/java/com/mopl/mopl_batch/batch/batch/sport/listener/SportStepListener.java
@@ -4,16 +4,12 @@ import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.StepExecutionListener;
 
-import com.mopl.mopl_batch.batch.entity.Type;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RequiredArgsConstructor
 public class SportStepListener implements StepExecutionListener {
-
-	private final Type contentType;
 
 	@Override
 	public void beforeStep(StepExecution stepExecution) {
@@ -22,8 +18,7 @@ public class SportStepListener implements StepExecutionListener {
 
 	@Override
 	public ExitStatus afterStep(StepExecution stepExecution) {
-		log.info("[SportStepListener][Step 완료 - Type: {}, Read: {}, Write: {}]",
-			contentType,
+		log.info("[SportStepListener][Step 완료 - Read: {}, Write: {}]",
 			stepExecution.getReadCount(),
 			stepExecution.getWriteCount()
 		);
